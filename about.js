3e71d4ab08f4d19bc0a396c2cedfe5faa58d312f
perrinni <nick.perrin@mail.utoronto.ca>
2020-05-03 14:40:34 -0400

    Got cueing of specific CSS anims on scroll triggered by JS working

--- about.js
+++ about.js
@@ -1,8 +1,6 @@
-/* ------------------
-   ABOUT PAGE script
-   ----------------- */
-
-// Going with a hard-coded approach for now, as this page is not likely to expand.
+// ------------------------------------
+// ABOUT PAGE script
+// ------------------------------------
 
 // Get viewport height and width
 const docElement =
@@ -12,29 +10,52 @@ const docElement =
 const viewportHeight = docElement.clientHeight;
 const viewportWidth = docElement.clientWidth;
 
-// Make array of all temporarily hidden elements
-// TODO: Get skills section and footnote in here
-let hiddenElements = [document.getElementById("about_education")];
+// Collect all sections. Hard-coding values as this page is not likely to expand.
+let educationSection = {
+  elem: document.getElementById("about_education"),
+  showClass: "show-education",
+};
+let skillsSection = {
+  elem: document.getElementById("about_skills"),
+  showClass: "show-skills",
+};
+let footnote = {
+  elem: document.getElementById("about_footnote"),
+  showClass: "show-footnote",
+};
+const sections = [educationSection, skillsSection, footnote];
 
 // Listen for scroll events
 document.addEventListener("scroll", appear);
 
-// Make hidden elements appear if they're already in view
-if (hiddenElements[0].getBoundingClientRect().top <= viewportHeight) {
-  hiddenElements[0].classList.add("show");
-  hiddenElements[0].classList.remove("hide");
-  // TODO: remove this element from the array
+// TODO: this whole for loop thing is called multiple times. Put it in a separate callable function
+// Make hidden elements appear if they're ALREADY in view
+for (let i = 0; i < sections.length; i++) {
+  const section = sections[i];
+  if (section.elem.getBoundingClientRect().top <= viewportHeight) {
+    section.elem.classList.remove("hide");
+    section.elem.classList.add(section.showClass);
+    if (i === sections.length - 1) {
+      document.removeEventListener("scroll", appear);
+    }
+  }
 }
 
-// Make div with id "about_education" change class when scrolled into view
+// Make hidden divs change class when SCROLLED into view
 function appear() {
-  const rect = hiddenElements[0].getBoundingClientRect();
-  /* Debugging */
+  // Debugging
   // console.log("rect.top: " + rect.top);
   // console.log("viewportHeight: " + viewportHeight);
-  if (rect.top <= viewportHeight) {
-    hiddenElements[0].classList.add("show");
-    hiddenElements[0].classList.remove("hide");
-    document.removeEventListener("scroll", appear);
+
+  for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    if (section.elem.getBoundingClientRect().top + 10 <= viewportHeight) {
+      // TODO: do some (top - bottom) thing to make it appear a little later?
+      section.elem.classList.remove("hide");
+      section.elem.classList.add(section.showClass);
+      if (i === sections.length - 1) {
+        document.removeEventListener("scroll", appear);
+      }
+    }
   }
 }

4f59c766bbd3581e92c19fc503f378e72872c65f
perrinni <nick.perrin@mail.utoronto.ca>
2020-06-17 16:58:37 -0400

    portfolio updates

--- portfolio.js
+++ portfolio.js
@@ -26,32 +26,65 @@ const games = [
     linkTexts: ["Play on itch.io", "Download Windows executable"],
   },
   {
-    name: "Mr. Placeholder",
-    platform: "PlaceHeld",
-    images: ["images/index_placeholder.jpg", "images/index_placeholder2.jpg"],
-    video: [],
-    blurbLines: ["I am a placeholder.", "That's right - I hold a place!"],
-    linkURLs: [
-      "https://www.google.ca/",
-      "https://www.rockpapershotgun.com/",
-      "images/logo.png",
+    name: "LOONY LAB",
+    platform: "Unity",
+    images: [
+      "images/games/ll/ll01.png",
+      "images/games/ll/ll02.png",
+      "images/games/ll/ll03.png",
+      "images/games/ll/ll04.png",
     ],
-    linkTexts: ["Go to Google", "Go to RPS", "Go to local image"],
-  },
-  {
-    name: "Mr. Placeholder2",
-    platform: "PlaceHeld2",
-    images: ["images/logo.png"],
     video: [],
     blurbLines: [
-      "Can't stop holding",
-      "",
-      "this place",
-      "",
-      "Too much to lose!",
+      "Loony Lab was part of a research project headed by Steve Engels at the University of Toronto, studying the effectiveness of design changes in pedagogical techniques in videogames. The material was taken straight from high school curriculum.",
+      "Working with a partner, we created a game design document, recorded progress in a dev blog, ran playtests with various demographics including game industry professionals, conducted two rounds of research with the game using self-developed study instruments, and produced a final report on our findings.",
+      "I contributed code (C#), design, art, animation, and sound. Royalty-free visual assets were modified to suit our needs.",
     ],
-    linkURLs: ["https://www.amazon.ca/"],
-    linkTexts: ["Amazon"],
+    linkURLs: [
+      "https://www.google.ca/", // TODO: real winx64 build link pLZ
+      "https://github.com/NPTP/LoonyLab",
+    ],
+    linkTexts: ["Download for Winx64", "Go to Github repo"],
+  },
+  {
+    name: "GAME JAMS",
+    platform: "HTML5 / Javascript",
+    images: [
+      "images/games/gj/gj01.png",
+      "images/games/gj/gj02.png",
+      "images/games/gj/gj03.png",
+      "images/games/gj/gj04.png",
+    ],
+    video: [],
+    blurbLines: [
+      `For several game jams in Toronto, I've worked with a team consisting of a designer, an artist and a programmer, all of whom have experience either in AAA or indie game production. They're a truly talented bunch and it's been a privilege to do these jams with them under the team banner of "ButterBrains."`,
+      "We put or heads together to create the high-level designs for these games as a group. I did all sound and music, and some level design.",
+    ],
+    linkURLs: [
+      "https://butterbrains.itch.io/relapse",
+      "https://butterbrains.itch.io/jelly-jelly",
+      "https://butterbrains.itch.io/battlebeans",
+      "https://globalgamejam.org/2016/games/combust-move",
+    ],
+    linkTexts: [
+      "Play (R)elapse on itch.io",
+      "Play Jelly-Jelly on itch.io",
+      "Play BattleBeans (2-player) on itch.io",
+      "Play Combust-a-Move (2-player) on GGJ",
+    ],
+  },
+  {
+    name: "INDUSTRIAL SECTOR",
+    platform: "GZDoom",
+    images: ["images/games/is/is01.png"],
+    video: [],
+    blurbLines: [
+      "A high-concept, grim post-apocalyptic mod for Doom (1993) built on a modern port of the game's open-source engine.",
+      "A work-in-progress passion project where I produce code, mapping, level design, music, sound design, animation, and narrative, while keeping the core of the game's assets and design in the classic Doom mould.",
+      "It's also using existing resources and tools made available by people in the incredible community around DooM that is still flourishing today.",
+    ],
+    linkURLs: [],
+    linkTexts: [],
   },
 ];
 

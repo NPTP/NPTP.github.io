836cd091321a2c3dae37745753522de8bb98d694
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-09-11 17:27:03 -0400

    Update how about page appears in script

--- about.js
+++ about.js
@@ -22,29 +22,54 @@ let displaying = false;
 document.addEventListener("scroll", appear);
 
 function reveal(cardList, index) {
-  cardList[index].classList.remove("hide-card");
-  cardList[index].classList.add("card");
-  cardList[index].classList.add("card-appear");
+  let revealTimeout = 0;
+  if (cardList[index].id === "0") {
+    blurbContainer = cardList[0];
+    blurbContainer.classList.remove("hide");
+    blurbContainer.classList.add("about-container");
+    let leftSide = document.querySelector("#left-about-id");
+    let rightSide = document.querySelector("#right-about-id");
+    let footnote = document.querySelector("#footnote");
+    leftSide.classList.remove("hide");
+    rightSide.classList.remove("hide");
+    footnote.classList.remove("hide");
+    leftSide.classList.add("left-about");
+    leftSide.classList.add("left-about-anim");
+    rightSide.classList.add("right-about");
+    rightSide.classList.add("right-about-anim");
+    footnote.classList.add("about-buildnote");
+    footnote.classList.add("vwtext-16");
+    revealTimeout = 1750;
+  } else {
+    cardList[index].classList.remove("hide-card");
+    cardList[index].classList.add("card");
+    cardList[index].classList.add("card-appear");
+    revealTimeout = CARDREVEAL_TIMEOUT_VAL;
+  }
 
   if (index < cardList.length - 1) {
     delay = setTimeout(function () {
       reveal(cardList, index + 1);
-    }, CARDREVEAL_TIMEOUT_VAL);
+    }, revealTimeout);
   }
 }
 
 // Make hidden divs change class when SCROLLED into view
 function appear() {
   cardList = [];
-  for (let i = 1; i < NUM_ABOUT_CARDS; i++) {
+  for (let i = 0; i < NUM_ABOUT_CARDS; i++) {
     const card = document.getElementById(i.toString());
-    const top = card.getBoundingClientRect().top;
-    if (top <= viewportHeight) {
-      if (card.className === "hide-card") {
-        cardList.push(card);
-      }
-      if (i === NUM_ABOUT_CARDS - 1) {
-        document.removeEventListener("scroll", appear);
+    if (i === 0 && card.className === "hide") {
+      cardList.push(card);
+    } else {
+      const top = card.getBoundingClientRect().top;
+      if (top <= viewportHeight) {
+        if (card.className === "hide-card") {
+          cardList.push(card);
+        }
+        if (i === NUM_ABOUT_CARDS - 1) {
+          document.removeEventListener("scroll", appear);
+        }
       }
     }
   }
